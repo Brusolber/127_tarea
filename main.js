e20d324a5c4062be57e91212d81f8ce17c3c9794
Brusolber <105524705+Brusolber@users.noreply.github.com>
2023-03-06 19:37:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function preload() {
         song=loadSound("music.mp3");
@@ -6,6 +10,8 @@ function preload() {
 
 function play() {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
 
 function setup() {
@@ -14,6 +20,27 @@ function setup() {
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+console.log('PoseNet esta inicializado');
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+ leftWristX +" leftWristY= "+ leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("righttWristX = "+ rightWristX +" rightWristY= "+ rightWristY);
+    }
 }
 
 function draw() {
